1f6e66f57550b9c80dedd5098e448ac39d717519
asim choudhury <asim choudhury>
2016-11-03 00:24:13 +0530

    my first commit

---
+++ MyWorld.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	// My first hello world code
+	
+	# Another way to comment
+	
+	/* 
+	 * Same as Java for multiple line comments
+	 * */
+	echo "Hello Atmaram!!!<br>";
+	
+	# Practicing My mathematics here
+	$x = 5 + 20;
+	echo "Summation of 5 and 20 is " . $x . "<br>";
+	$x= $y = "Cartoon";
+	echo "Testing: ". $x . "<br>";
+	#Practicing String variables
+	$color = "red";
+	echo "My House has many colors of <b>$color</b> <br>";
+	
+?>
+</body>
+</html>
